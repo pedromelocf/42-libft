ae4a82003dce36f98df309000c24c323df55617b
Pedro Melo <pedromelocf@yahoo.com>
2023-08-01 15:46:20 -0300

    I fixed three errors in this function. The variable type *x was wrong. I just assigned it to an const char variable. Then, I fixed the verification in while loop, verifing if the parameter was above ascii table, and in this case, returning the s string itself. Third, same as strchr, I fixed the return path, witch was returning an adress to the variable,not a pointer to the variable adress, as strrchr does.

--- ft_str/ft_strrchr.c
+++ ft_str/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 13:00:42 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/07/31 14:39:46 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/01 15:33:25 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	size_t	i;
-	size_t	*x;
+	const char *x;
 
 	i = 0;
 	x = (void *)0;
-	while (s[i])
+	while (s[i] && c <= 126)
 	{
 		if (s[i] == c && c <= 126)
 			x = &s[i];
 		i++;
 	}
 	if (c == '\0' || c >= 127)
-		return (&s[i]);
-	return (x);
+		return (char*)&s[i];
+	return (char*)x;
 }
