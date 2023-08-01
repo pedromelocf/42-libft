22e34f9344a9fb4e717e6fef25767e7205907c34
Pedro Melo <pedromelocf@yahoo.com>
2023-08-01 18:00:44 -0300

    Adjusting function bzero. I just set a char pointer and associated this char pointer to the parameter type void *s. It happens because we cant acess or modify the value type void before convert it to the right type char.

--- ft_mem/ft_bzero.c
+++ ft_mem/ft_bzero.c
@@ -6,18 +6,21 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 10:39:06 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/07/27 10:44:54 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/01 17:52:42 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <string.h>
 
 void	ft_bzero(void *s, size_t n)
 {
 	size_t	counter;
+	char* ptr = s;
 
 	counter = 0;
 	while (counter < n)
 	{
-		(s + counter) = '\0';
+		ptr[counter] = '\0';
 		counter++;
 	}
 	return ;
