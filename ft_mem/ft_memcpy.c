05635422649d3d5a543308f7165419e7374c9624
Pedro Melo <pedromelocf@yahoo.com>
2023-08-01 18:03:36 -0300

    Adjusting function bzero. I just set a char pointer ptrdest and a const char ptrsrc pointer.I associated this char pointer to the parameter type void *s. It happens because we cant acess or modify the value type void before convert it to the right type char.

--- ft_mem/ft_memcpy.c
+++ ft_mem/ft_memcpy.c
@@ -6,18 +6,24 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 10:55:32 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/07/27 10:59:52 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/01 17:54:08 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <string.h>
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	counter;
+	char* ptrdest;
+	const char* ptrsrc;
 
 	counter = 0;
+	ptrdest = dest;
+	ptrsrc = src;
 	while (counter < n)
 	{
-		(dest + counter) = (src + counter);
+		ptrdest[counter] = ptrsrc[counter];
 		counter++;
 	}
 	return (dest);
