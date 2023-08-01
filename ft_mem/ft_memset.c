9ac457eb862298ac031f8c2dca46da3072ae7619
Pedro Melo <pedromelocf@yahoo.com>
2023-08-01 18:04:51 -0300

    Adjusting function memset. I just set a char pointer .I associated this char pointer to the parameter type void *s. It happens because we cant acess or modify the value type void before convert it to the right type char.

--- ft_mem/ft_memset.c
+++ ft_mem/ft_memset.c
@@ -6,18 +6,22 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 10:10:28 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/07/27 10:39:23 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/01 17:54:53 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <string.h>
 
 void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t	counter;
+	char* ptr;
 
 	counter = 0;
+	ptr = s;
 	while (counter < n)
 	{
-		(s + counter) = c;
+		ptr[counter] = c;
 		counter++;
 	}
 	return (s);
