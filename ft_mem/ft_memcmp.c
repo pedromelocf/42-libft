d939713aee0b6a7c6e76a0f70c930202d4d74759
Pedro Melo <pedromelocf@yahoo.com>
2023-08-03 15:01:03 -0300

    Fixed two erros. First one is verification, it is not necessary for this function (was causing errors because only verified if the first memory position was 0). Second, i fixed the variable pointer and set it to an unsigned const char, as the function behave.

--- ft_mem/ft_memcmp.c
+++ ft_mem/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 16:32:07 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/02 16:42:35 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/03 14:56:43 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,17 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const char	*ptrs1;
-	const char	*ptrs2;
+	unsigned const char	*ptrs1;
+	unsigned const char	*ptrs2;
 	int			i;
 
 	ptrs1 = s1;
 	ptrs2 = s2;
 	i = 0;
-	if (!ptrs1[i] || !ptrs1[i])
-		return (0);
 	while (i < n)
 	{
 		if (ptrs1[i] != ptrs2[i])
-			return ((int)(ptrs1 - ptrs2));
+			return (int)(ptrs1[i] - ptrs2[i]);
 		i++;
 	}
 	return (0);
