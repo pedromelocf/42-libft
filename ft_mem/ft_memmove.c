e9725ba6ade846724a532a8328c8f7ba400a45e1
Pedro Melo <pedromelocf@yahoo.com>
2023-08-04 10:25:48 -0300

    First ft_memmove version. 1 test failed

--- ft_mem/ft_memmove.c
+++ ft_mem/ft_memmove.c
@@ -6,11 +6,25 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 11:03:13 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/07/27 11:03:46 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/04 10:24:59 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	
+	char *ptrdest;
+	const char *ptrsrc;
+	size_t i;
+
+	ptrdest = dest;
+	ptrsrc = src;
+	i = 0;
+	while (i < n)
+	{
+		ptrdest[i] = ptrsrc[i];
+		i++;
+	}
+	return (0);
 }
