c6789c13c699dc332e344953f7bf04de02cd8126
Pedro Melo <pedromelocf@yahoo.com>
2023-08-04 13:39:31 -0300

    Fixing function. Created dest and source lenght verification. If dest greater than src, use last to first bytes copyes (avoid data losses and its more secure when overlap happens).

--- ft_mem/ft_memmove.c
+++ ft_mem/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 11:03:13 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/04 10:24:59 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/04 13:35:55 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,29 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char *ptrdest;
-	const char *ptrsrc;
-	size_t i;
+	char	*ptrdest;
+	const char	*ptrsrc;
+	size_t	i;
 
-	ptrdest = dest;
+	ptrdest =dest;
 	ptrsrc = src;
 	i = 0;
-	while (i < n)
+	if (dest <= src)
 	{
-		ptrdest[i] = ptrsrc[i];
-		i++;
+	while (i < n)
+		{
+			ptrdest[i] = ptrsrc[i];
+			i++;
+		}
 	}
-	return (0);
+	if (dest > src)
+	{
+		i = n;
+		while (i > 0)
+		{
+			ptrdest[i - 1] = ptrsrc[i - 1];
+			i--;
+		}
+	}
+	return (dest);
 }
