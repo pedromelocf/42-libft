cdec936f68d44d5281316c81d34d48e22cd2e03b
Pedro Melo <pedromelocf@yahoo.com>
2023-08-07 17:23:06 -0300

    Fixing libt.h include; insert two verifications: 1 - If the result from division are not as expected (indicates an integer overflow) return error(null); 2 - If nmemb or size equals zero, return malloc for then sucessfully be passed for free.

--- part1/ft_mem/ft_calloc.c
+++ part1/ft_mem/ft_calloc.c
@@ -6,26 +6,31 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 13:15:22 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/05 13:11:36 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/07 17:20:16 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdlib.h>
+#include "libft.h"
 
 void *ft_calloc(size_t nmemb, size_t size)
 {
-	char *memmory;
+	char *array;
 	size_t index;
+	size_t maxint_validator;
 
 	index = 0;
-	memmory = (void *)malloc(size * nmemb);
-	if (memmory == NULL)
-		return(0);
+	maxint_validator = (nmemb * size);
+	if (nmemb == 0 || size == 0)
+		return(malloc(0));
+	if (maxint_validator / nmemb != size)
+		return (NULL);
+	array = (void *)malloc(size * nmemb);
+	if (array == NULL)
+		return (NULL);
 	while (index < nmemb * size)
 	{
-		memmory[index] = '\0';
+		array[index] = '\0';
 		index++;
 	}
-	return(memmory);
+	return (array);
 }
