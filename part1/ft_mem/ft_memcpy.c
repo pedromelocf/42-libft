f03ce130b2a58f31d24344a3cc05e375c8b627e9
pmelo-ca <pedromelocf@yahoo.com>
2023-08-08 11:18:47 -0300

    Fixing return issues when dest and src are both null (based on francinette test).

--- part1/ft_mem/ft_memcpy.c
+++ part1/ft_mem/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 10:55:32 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/02 11:04:24 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/08 11:17:44 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	counter = 0;
 	ptrdest = dest;
 	ptrsrc = src;
+	if (ptrdest == NULL && ptrsrc == NULL)
+		return (NULL);
 	while (counter < n)
 	{
 		ptrdest[counter] = ptrsrc[counter];
