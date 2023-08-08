c5dda34e223e71bd579e5cb82a5866febb319c71
Pedro Melo <pedromelocf@yahoo.com>
2023-08-07 22:16:36 -0300

    Fixing some validations. Still not working 100%.

--- part2/ft_substr.c
+++ part2/ft_substr.c
@@ -1,17 +1,21 @@
-
-#include <string.h>
-#include <stdlib.h>
+#include "libft.h"
 
 char *ft_substr(char const *s, unsigned int start,size_t len)
 {
 	char *substring;
 	size_t counter;
+	size_t s_len;
 
 	counter = 0;
-	substring = malloc(sizeof(char) * len  + 1);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (malloc(1));
+	if (len > s_len - start)
+		len = s_len - start;
+	substring = malloc(sizeof(char) * (len  + 1));
 	if (substring == NULL)
 		return (NULL);
-	while(s[counter] && counter < len)
+	while(counter < len)
 	{
 		substring[counter] = s[start + counter];
 		counter++;
