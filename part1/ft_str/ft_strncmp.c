9b1c5e9cb86819c24a01df2114a52fb38e091c8b
Pedro Melo <pedromelocf@yahoo.com>
2023-08-09 11:46:13 -0300

    Fixing casting from size_t to unsigned char based on francinette tests. I also fixed the variable j from type size_t t int (sometimes j can be lower than 0, and usingsize_t variable to storage this can cause errors).

--- part1/ft_str/ft_strncmp.c
+++ part1/ft_str/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/27 10:13:46 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/02 11:03:44 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/09 11:43:44 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
-	size_t	j;
+	int	j;
 
 	i = 0;
 	j = 0;
 	while ((s1[i] && s2[i]) && (i < n) && !j)
 	{
-		j = ((size_t)s1[i] - (size_t)s2[i]);
+		j = ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	if ((i < n) && s1[i] != s2[i] && !j)
-		j = ((size_t)s1[i] - (size_t)s2[i]);
+		j = ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (j);
 }
