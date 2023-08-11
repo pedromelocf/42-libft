4175180e7c714145ff963b5d2a975480977ee8b6
pmelo-ca <pedromelocf@yahoo.com>
2023-08-11 14:41:27 -0300

    Adding ft_putstr_fd.c

--- part2/ft_putstr_fd.c
+++ part2/ft_putstr_fd.c
@@ -6,13 +6,13 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 11:57:01 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/11 12:16:53 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/11 14:41:03 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-ft_putstr_fd(char *s, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-	
+	write(fd, &*s, ft_strlen(s));
 }
