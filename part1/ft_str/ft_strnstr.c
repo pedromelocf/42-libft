1c5f889583f74a884d2a4116f378f4cce3a825f7
Pedro Melo <pedromelocf@yahoo.com>
2023-08-12 19:25:55 -0300

    Fixing fuction params.

--- part1/ft_str/ft_strnstr.c
+++ part1/ft_str/ft_strnstr.c
@@ -6,13 +6,13 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 09:09:21 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/11 15:38:01 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/12 19:25:02 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(char *big, const char *little, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
