6e8eca96d406d82ad365bb658f8a0cda36e18d0c
Pedro Melo <pedromelocf@yahoo.com>
2023-08-14 20:56:51 -0300

    Inserting strmapi function. I protected when the memmory allocation is null and I set the last position in string to 0. I use the function strlen to count the number of chars in the string and them created one loop to insert the result from new functions to the specific position in string array.

--- part2/ft_strmapi.c
+++ part2/ft_strmapi.c
@@ -6,12 +6,30 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 11:58:29 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/11 12:16:03 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/14 20:51:51 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	size_t counter;
+	size_t index;
+	char *string;
+
+	index = 0;
+	counter = ft_strlen(s);
+	string = (char *)malloc(sizeof(char) * counter + 1);
+	if (string == NULL)
+		return (NULL);
+	string[counter] = '\0';
+	while (index < counter)
+	{
+		string[index] = f(index, s[index]);
+		index++;
+	}
+	return (string);
 }
