0d8ed19a8930b45f1f86da3013a2b0c4b489d3eb
Pedro Melo <pedromelocf@yahoo.com>
2023-08-18 10:16:09 -0300

    Fixing some errors, and spliting the solution. Still need some adjustments.

--- part2/ft_split.c
+++ part2/ft_split.c
@@ -6,19 +6,7 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 11:58:59 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/18 00:26:46 by pmelo-ca         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/11 11:58:59 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/18 00:25:59 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/18 10:13:21 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,19 +16,46 @@
 char	**ft_count_char_c(char const *s, char c, char **splited_array)
 {
 	size_t arrays_counter;
+	size_t s_index;
 
+	s_index = 0;
 	arrays_counter = 1;
-	while (*s)
+	while (s[s_index])
 	{
-		if (*s == c)
+		if (s[s_index] == c)
 			arrays_counter++;
-		s++;
+		s_index++;
 	}
 	splited_array = (char **)malloc(sizeof(char *) * arrays_counter);
 	return (splited_array);
 }
 
-char **ft_set_alloc_arrays(char const *s, char c, char **splited_array)
+char **ft_alloc_arrays(char const *s, char c, char **splited_array)
+{
+	size_t arrays_index;
+	size_t string_index;
+	size_t s_index;
+
+	arrays_index = 0;
+	string_index = 0;
+	s_index = 0;
+
+	while (s[s_index])
+	{
+		if (s[s_index] == c)
+		{
+			splited_array[arrays_index] = (char*)malloc(sizeof(char) * string_index + 1);
+			arrays_index++;
+			string_index = 0;
+		}
+		else
+			string_index++;
+		s_index++;
+	}
+	splited_array[arrays_index] = (char*)malloc(sizeof(char) * string_index + 1);
+	return (splited_array);
+}
+char **ft_set_arrays(char const *s, char c, char **splited_array)
 {
 	size_t arrays_index;
 	size_t string_index;
@@ -51,18 +66,20 @@ char **ft_set_alloc_arrays(char const *s, char c, char **splited_array)
 	s_index = 0;
 	while (s[s_index])
 	{
-		if (s[string_index] == c)
+		if (s[s_index] == c)
 		{
-			splited_array[arrays_index] = (char*)malloc(sizeof(char) * (string_index + 1));
 			splited_array[arrays_index][string_index] = '\0';
 			arrays_index++;
 			string_index = 0;
 		}
-		splited_array[arrays_index] = (char*)malloc(sizeof(char) * (string_index + 1));
-		splited_array[arrays_index][string_index] = s[string_index];
-		string_index++;
+		else
+		{
+			splited_array[arrays_index][string_index] = s[s_index];
+			string_index++;
+		}
 		s_index++;
 	}
+	splited_array[arrays_index][string_index] = '\0';
 	return (splited_array);
 }
 
@@ -70,27 +87,10 @@ char	**ft_split(char const *s, char c)
 {
 	char **splited_array;
 
-	splited_array = 0;
-	printf("Hello\n");
 	splited_array = ft_count_char_c(s, c, splited_array);
-	printf("Hello\n");
-	splited_array = ft_set_alloc_arrays(s, c, splited_array);
-	printf("Hello\n");
+	if (!(ft_alloc_arrays(s, c, splited_array)))
+		return (NULL);
+	splited_array = ft_alloc_arrays(s, c, splited_array);
+	splited_array = ft_set_arrays(s, c, splited_array);
 	return (splited_array);
 }
-
-int main (void)
-{
-	char *string;
-	char c;
-	char **result;
-	int index;
-
-	index = 0;
-	string = "Taste";
-	c = 'e';
-	result = ft_split(string, c);
-	printf("Resultado esperado: Teste\n");
-	printf("Resultado obtido: %s\n", result[index]);
-}
-
