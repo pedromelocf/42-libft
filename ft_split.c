5d2f438cc695ac88c3bda55350ac916d066ce9e9
Pedro Melo <pedromelocf@yahoo.com>
2023-08-20 10:00:06 -0300

    Fixing split. Working but need to decrease line function

--- ft_split.c
+++ ft_split.c
@@ -6,14 +6,14 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 11:58:59 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/19 19:36:24 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/20 09:58:57 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-char	**ft_count_char_c(char const *s, char c, char **splited_array)
+static	**ft_count_substrings(char const *s, char c, char **splited_array)
 {
 	size_t	arrays_counter;
 	size_t	s_index;
@@ -26,9 +26,9 @@ char	**ft_count_char_c(char const *s, char c, char **splited_array)
 	{
 		if (s[s_index] == c)
 			s_index++;
-		else if (s[s_index != c])
+		else if (s[s_index] != c)
 		{
-			if (s[s_index + 1] == c)
+			if (s[s_index + 1] == c || s[s_index + 1] == '\0')
 				arrays_counter++;
 			s_index++;
 		}
@@ -40,46 +40,36 @@ char	**ft_count_char_c(char const *s, char c, char **splited_array)
 char	**ft_split(char const *s, char c)
 {
 	char	**splited_array;
+	char	*start;
 	size_t	index;
 	size_t	substring_len;
-	size_t	test;
 
-	test = 0;
-	splited_array = ft_count_char_c(s, c, splited_array);
 	index = 0;
+	start = (char *)s;
+	splited_array = ft_count_substrings(s, c, splited_array);
 	substring_len = ft_strlen(s);
 	if (s == NULL || splited_array == NULL)
 		return (NULL);
-	while (s[index])
+	while (*s)
 	{
-		while (s[index] == c && s[index])
-			index++;
-		if (s[index])
+		if (*s == c)
 		{
-			if (ft_strchr(s, c) != 0)
-				substring_len = ft_strlen(s);
-			else if (*s != c)
-				substring_len = ft_strchr(s, c) - s;
-			printf("substring_len : %zu\n", substring_len);
-			splited_array[test] = ft_substr(s, 0, substring_len);
-			index += substring_len;
+			if (s > start)
+			{
+				substring_len = s - start;
+				splited_array[index] = ft_substr(start, 0, substring_len);
+				index++;
+			}
+			start = (char *)(s + 1);
 		}
+		s++;
 	}
-	splited_array[test] = NULL;
+	if (s > start)
+	{
+		substring_len = s - start;
+		splited_array[index] = ft_substr(start, 0, substring_len);
+		index++;
+	}
+	splited_array[index] = NULL;
 	return (splited_array);
-}
-int main (void)
-{
-	char *string;
-	char c;
-	char **result;
-
-	string = "     ";
-	c = 'e';
-	result = ft_split(string, c);
-	printf("Resultado esperado: Pedro\n");
-	printf("Resultado obtido: %s\n", result[0]);
-	// printf("Resultado obtido: %s\n", result[1]);
-	// printf("Resultado obtido: %s\n", result[2]);
-	// printf("Resultado obtido: %s\n", result[3]);
 }
