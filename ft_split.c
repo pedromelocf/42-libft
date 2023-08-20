9d7b7118cb8271b5ea64dfa06def1c7b1f3b56c3
pmelo-ca <pedromelocf@yahoo.com>
2023-08-20 13:29:04 -0300

    Adding static function and removing wrong include

--- ft_split.c
+++ ft_split.c
@@ -6,14 +6,13 @@
 /*   By: pmelo-ca <pmelo-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 11:58:59 by pmelo-ca          #+#    #+#             */
-/*   Updated: 2023/08/20 10:08:15 by pmelo-ca         ###   ########.fr       */
+/*   Updated: 2023/08/20 13:22:31 by pmelo-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-char	**ft_count_substrings(char const *s, char c, char **splited_array)
+static char	**ft_count_substrings(char const *s, char c, char **splited_array)
 {
 	size_t	arrays_counter;
 	size_t	s_index;
